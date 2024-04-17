965727a2cebfc79eeaa1d4b8c04dd96cac4ac8c7
Bhaskar Kumar Mondal <75377209+bhaskar9221@users.noreply.github.com>
2024-04-17 17:45:55 +0530

    Add files via upload

---
+++ TweetDOM.py
@@ -0,0 +1,11 @@
+from ntscraper import Nitter
+import pandas as pd
+scraper = Nitter()
+username = "elonmusk"
+number_of_tweets = 5
+tweets = scraper.get_tweets(username,mode="user",number=number_of_tweets)
+tweets_list = []
+for tweet in tweets["tweets"] :
+  tweets_list.append( {"tweet" : tweet['text']})
+df = pd.DataFrame(tweets_list)
+df.head()
